8eae90c731d7426f27121eee2f3f14f4472cf2b7
Attila Horvath <sweeptic@gmail.com>
2025-03-06 14:50:34 +0100

    003 User Signup Extracting & Validating User Input

---
+++ actions/auth.js
@@ -0,0 +1,22 @@
+'use server';
+
+export async function signup(prevState, formdata) {
+  const email = formdata.get('email');
+  const password = formdata.get('password');
+
+  let errors = {};
+
+  if (!email.includes('@')) {
+    errors.email = 'Please enter a valid email address';
+  }
+
+  if (password.trim().length < 8) {
+    errors.password = 'Password must be at least 8 characters long.';
+  }
+
+  // store it in the database ( create a new user)
+
+  if (Object.keys(errors).length > 0) {
+    return { errors };
+  }
+}
