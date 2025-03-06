e3282184f06a7f53d2e06cef7cadc328a2fa5a85
Attila Horvath <sweeptic@gmail.com>
2025-03-06 18:50:08 +0100

    fix html problem

--- app/(auth)/layout.js
+++ app/(auth)/layout.js
@@ -8,16 +8,18 @@ export const metadata = {
 
 export default function AuthRootLayout({ children }) {
   return (
-    <html lang="en">
-      <body>
-        <header id="auth-header">
-          <p>Welcome back!</p>
-          <form action={logout}>
-            <button>Logout</button>
-          </form>
-        </header>
-        {children}
-      </body>
-    </html>
+    // <html lang="en">
+    //   <body>
+    <>
+      <header id="auth-header">
+        <p>Welcome back!</p>
+        <form action={logout}>
+          <button>Logout</button>
+        </form>
+      </header>
+      {children}
+    </>
+    //   </body>
+    // </html>
   );
 }
